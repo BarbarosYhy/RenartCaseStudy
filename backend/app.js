705467bd536f1038ff33e3184cf95285209ec90c
BarbarosYhy <barbaros.acet@sabanciuniv.edu>
2025-10-03 21:34:44 +0300

    Real-time gold price api added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const fs = require ('fs');
+
 const app = express();
 
 app.listen(3000);
 
-app.get('/', (req,res) => {
-    res.render('welcome',);
+
+const products = require('./products/products.json');
+
+
+app.get('/', async (req,res) => {
+   console.log("Hi World");
 });
+
+app.get('/products', (req,res) => {
+    res.json(products);
+})
+
+app.get('/goldPrice', async (req,res) => {
+    const goldPriceUrl = "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD";
+    const response = await fetch(goldPriceUrl);
+    const data = await response.json();
+    const goldPrice = data[0].spreadProfilePrices[0].bid;
+    res.json(goldPrice);
+})
