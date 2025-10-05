14e4568319792cd218689eb809e2de9ac83c38d0
BarbarosYhy <barbaros.acet@sabanciuniv.edu>
2025-10-05 16:19:29 +0300

    Frontend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173'   // Vite dev server
+}));
 
 app.listen(3000);
 
